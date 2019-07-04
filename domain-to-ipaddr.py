17bf9bac18dd0e4f5f68d55771acf2fefb6757b4
Antonius Yonathan <atomicbomber.email@gmail.com>
2019-07-04 10:21:03 +0700

    Now produces more than 1 address.

--- domain-to-ipaddr.py
+++ domain-to-ipaddr.py
@@ -2,6 +2,10 @@ import socket
 import sys
 
 domain_name = sys.argv[1]
-ipv4_addr = socket.gethostbyname(domain_name)
+domain_info = socket.getaddrinfo(domain_name, 80)
 
-print(ipv4_addr)
+ipv4_addresses = []
+for item in domain_info:
+    ipv4_addresses.append(item[4][0])
+
+print(domain_name + " " + " ".join(ipv4_addresses))
